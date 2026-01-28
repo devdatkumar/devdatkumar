69b777386600dbfe3ab8a2f68aeff6a70872bae5
Devdat Kumar <devdatkumar.work@gmail.com>
2026-01-28 18:37:09 +0100

    vercel speed-insights and analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
@@ -35,6 +37,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights />
+          <Analytics mode="production" />
         </ThemeProvider>
       </body>
     </html>
